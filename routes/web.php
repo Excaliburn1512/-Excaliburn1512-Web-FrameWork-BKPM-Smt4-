035a1b7ab649c786455bfa326499616db1ee465e
Excaliburn1512 <igvimahendra@gmail.com>
2025-02-09 19:45:41 +0700

    acara 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user',[UserController::class,'index']);
+
+Route::match(['get','post'],'/',function(){
+    return 'ini match';
+});
+Route::any('/',function(){
+    return 'ini any';
+});
+Route::redirect('/here','there',301);
+Route::view('/welcome','welcome');
+Route::view('/welcome','welcome')->name('Taylor');
+Route::get('user/{name?}',function($name=null){
+    return $name;
+});
+Route::get('user/{name?}',function($name="John"){
+    return $name;
+});
+Route::get('user/{name}',function($name){
+
+})->where('name','[A-Za-z]+');
+Route::get('user/{id}',function($id){})->where('id','[0-9]+');
+Route::get('user/{id}{name}',function($id,$name){})->where(['id'=>'[0-9]+','name'=>'[a-z]+']);
+Route::get('user/{id}',function($id){
+
+});
+Route::get('search/{search}',function($search){
+    return $search;
+})->where('search','.*');
