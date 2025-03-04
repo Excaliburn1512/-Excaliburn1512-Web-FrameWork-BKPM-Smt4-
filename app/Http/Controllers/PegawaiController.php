fb4dfdc0141b9082703af3f4e76bb0064aa40992
Excaliburn1512 <igvimahendra@gmail.com>
2025-03-05 01:03:32 +0700

    acara 18

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -6,16 +6,29 @@ use Illuminate\Http\Request;
 
 class PegawaiController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         return $request->segment(2);
     }
-    public function formulir(){
+    public function formulir()
+    {
         return view('formulir');
     }
-    public function proses(Request $request){
-    $nama = $request->input('nama');
-    $alamat = $request->input('alamat');
+    public function proses(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|min:5|max:20',
+            'alamat' => 'required|alpha'
+        ], [
+            'nama.required' => 'Nama wajib diisi.',
+            'nama.min' => 'Nama harus minimal 5 karakter.',
+            'nama.max' => 'Nama harus maksimal 20 karakter.',
+            'alamat.required' => 'Alamat wajib diisi.'
+        ]);
 
-    return "Nama : ".$nama.", Alamat : ".$alamat;
+        $nama = $request->input('nama');
+        $alamat = $request->input('alamat');
+
+        return "Nama: " . $nama . ", Alamat: " . $alamat;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\backend\PengalamanKerjaController;
 use App\Http\Controllers\backend\PendidikanController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\CobaController;
 
 //Acara 3
 Route::get('/index', function () {
@@ -139,3 +140,5 @@ Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
 
 Route::get('/formulir', [PegawaiController::class, 'formulir']);
 Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/cobaerror/{nama?}', [CobaController::class, 'index']);
